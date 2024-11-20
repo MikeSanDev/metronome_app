b314dae05e3b7bc43013bcf299610ea22bf18953
MikeSanDev <mjs070594@gmail.com>
2024-11-20 08:57:48 -0600

    more functionality

--- src/metronome.js
+++ src/metronome.js
@@ -7,3 +7,6 @@ const startStopBtn = document.querySelector('.start-stop');
 const subtractBeats = document.querySelector('.subtract-beats'); 
 const addBeats = document.querySelector('.add-beats'); 
 const measureCount = document.querySelector('.measure-count'); 
+
+let bpm = 140;
+let beatsPerMeasure = 4; 
